8b676c1a5da8df63ecf08773ac8729168e50af2c
MadElf1337 <madelf1337@gmail.com>
2023-11-05 12:32:03 -0800

    inference edit

--- app.py
+++ app.py
@@ -3,13 +3,28 @@ import pandas as pd
 import numpy as np
 import pickle
 from PIL import Image
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader, Dataset
+import torchvision
+from torch import nn
+import os
 
 #Beautification
 st.set_page_config(page_title="AI Wound Analysis", page_icon="🧊", layout="wide", initial_sidebar_state="expanded")
 
 
 # Load model
-# model = pickle.load(open('model.pkl', 'rb')) 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = torchvision.models.convnext_base(pretrained=True)
+model.classifier = nn.Sequential(
+    torchvision.models.convnext.LayerNorm2d((1024,), eps=1e-06, elementwise_affine=True),
+    nn.Flatten(start_dim=1, end_dim=-1), 
+    nn.Linear(in_features=1024, out_features=11, bias=True)
+)
+model.load_state_dict(torch.load('model_weights_epoch79.pth'))
+model.eval()
+model.to(device)
 
 # Create title
 st.title('Wound Imaging Analysis Web App')
@@ -17,19 +32,29 @@ st.title('Wound Imaging Analysis Web App')
 # Upload image
 image = st.file_uploader('Upload wound image', type=['jpg','png'])
 
+def predict(model, input):
+    with torch.no_grad():
+        input = input.to(device)
+        output = model(input)
+        probabilities = torch.softmax(output, dim=1)
+        pred_class = torch.argmax(probabilities)
+        pred_label = {v: k for k, v in label_mapping.items()}[pred_class.item()]
+    return pred_label
+
 if image is not None:
 
     # Load image
     img = Image.open(image)
+    image = transform(image).unsqueeze(0) 
 
     # Preprocess image
     
     # Make prediction
-    # prediction = model.predict(img)
+    pred = predict(model, image)
     
     # Show image and prediction
     st.image(img)
-    st.success(f'Prediction: {prediction}')
+    st.success(f'Prediction: {pred}')
     
 else:
     st.info('Upload an image')
